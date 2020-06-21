be24ef960a2441b7a89d3357a9edc311efc62cfb
avolutions <acvogt89@gmail.com>
2020-06-21 19:18:47 +0200

    Create Container.php

---
+++ src/Di/Container.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * AVOLUTIONS
+ * 
+ * Just another open source PHP framework.
+ * 
+ * @copyright	Copyright (c) 2019 - 2020 AVOLUTIONS
+ * @license		MIT License (http://avolutions.org/license)
+ * @link		http://avolutions.org
+ */
+ 
+namespace Avolutions\Di;
+
+/**
+ * Container class
+ *
+ * The Dependency Injection Container.
+ *
+ * @author	Alexander Vogt <alexander.vogt@avolutions.org>
+ * @since	0.4.0
+ */
+class Container
+{
+    /**
+     * Finds an entry of the container by its identifier and returns it.
+     *
+     * @param string $name Identifier of the entry to look for.
+     *
+     * @return mixed The resolved entry.
+     */
+    public function get($name)
+    {
+        $parameters = [];
+
+        $ReflectionClass = new \ReflectionClass($name);        
+        $Constructor = $ReflectionClass->getConstructor();
+
+        if(!is_null($Constructor)) {
+            foreach ($Constructor->getParameters() AS $parameter) {
+                $className = $parameter->getType()->getName();
+                $parameters[] = $this->get($className);
+            }
+        }
+
+        return new $name(...$parameters);
+    }
+}
